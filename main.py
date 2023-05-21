c3271d82ac217228ca33d3b8f272ddeb2a391f18
PiotrKrzem <piotr.krzeminski@intel.com>
2023-05-21 15:36:31 +0200

    [FIX] Concatenate 2 losses into 1, custom loss function

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn.functional as F
 
-NR_OF_CARDS = 1
+NR_OF_CARDS = 10
 NR_OF_BUCKETS = 1
 NR_OF_CARDS_PER_DECK = 4
+
 ENCODER_DIM = 3
 DENSE_NEURONS = 16
-LEARNING_RATE = 1e-6
+LEARNING_RATE = 1e-4
 EPOCHS = 1000
 
 class ClashRoyaleNet(torch.nn.Module):
@@ -41,8 +42,10 @@ class ClashRoyaleNet(torch.nn.Module):
             bias=True
         )
 
-        self.missing_card_recovery_loss = torch.nn.CrossEntropyLoss()
-        self.deck_similarity_to_ranking_loss = torch.nn.BCEWithLogitsLoss()
+        # self.missing_card_recovery_loss = torch.nn.CrossEntropyLoss()
+        # self.deck_similarity_to_ranking_loss = torch.nn.BCEWithLogitsLoss()
+        # self.deck_similarity_to_ranking_loss = torch.nn.MSELoss()
+        # self.deck_similarity_to_ranking_loss = lambda x, y: (x - torch.min(x) - (y - torch.min(y))).sum()
 
     def forward(self, cards: torch.Tensor, ranking: torch.Tensor):
         """
@@ -56,8 +59,6 @@ class ClashRoyaleNet(torch.nn.Module):
         # -> (batch_size, nr_of_cards_per_deck, input_size)
         target_cards_one_hot = target_cards_one_hot.view([batch_size * self.nr_of_cards_per_deck, self.input_size]).float()
         # -> (batch_size * nr_of_cards_per_deck, input_size)
-        target_cards_softmax = target_cards_one_hot.softmax(dim=1)
-        # -> (batch_size * nr_of_cards_per_deck, input_size)
         target_cards_encoding = self.encoding_layer(target_cards_one_hot)
         # -> (batch_size * nr_of_cards_per_deck, encoding_size)
 
@@ -67,7 +68,7 @@ class ClashRoyaleNet(torch.nn.Module):
         # -> (batch_size, input_size)
         deck_encoding: torch.Tensor = self.encoding_layer(deck_one_hot)
         # -> (batch_size, encoding_size)
-        deck_encoding_repeat = deck_encoding.repeat(self.nr_of_cards_per_deck, 1).view((batch_size * self.nr_of_cards_per_deck, self.encoding_size))
+        deck_encoding = deck_encoding.repeat(self.nr_of_cards_per_deck, 1).view((batch_size * self.nr_of_cards_per_deck, self.encoding_size))
         # -> (batch_size * nr_of_cards_per_deck, encoding_size)
 
         offset_ranking = ranking.add(self.nr_of_cards)
@@ -78,28 +79,36 @@ class ClashRoyaleNet(torch.nn.Module):
         # -> (batch_size, input_size)
         ranking_encoding: torch.Tensor = self.encoding_layer(ranking_one_hot)
         # -> (batch_size, encoding_size)
-        # ranking_encoding = ranking_encoding.repeat(self.nr_of_cards_per_deck, 1).view((batch_size * self.nr_of_cards_per_deck, self.encoding_size))
+        ranking_encoding = ranking_encoding.repeat(self.nr_of_cards_per_deck, 1).view((batch_size * self.nr_of_cards_per_deck, self.encoding_size))
         # -> (batch_size * nr_of_cards_per_deck, encoding_size)
 
         # ========================================================================
-        deck_with_cards_removed = deck_encoding_repeat.subtract(target_cards_encoding)
+        deck_with_cards_removed = deck_encoding.subtract(target_cards_encoding)
         # -> (batch_size * nr_of_cards_per_deck, encoding_size)
-        deck_with_cards_removed = self.dense_layer_1(deck_with_cards_removed)
-        deck_with_cards_removed = F.tanh(deck_with_cards_removed)
+        predicted_removed_card = self.dense_layer_1(deck_with_cards_removed)
+        predicted_removed_card = F.relu(predicted_removed_card)
         # -> (batch_size * nr_of_cards_per_deck, nr_of_neurons_per_dense)
-        deck_with_cards_removed = self.dense_layer_2(deck_with_cards_removed)
-        deck_with_cards_removed = F.tanh(deck_with_cards_removed)
+        predicted_removed_card = self.dense_layer_2(predicted_removed_card)
+        predicted_removed_card = F.relu(predicted_removed_card)
         # -> (batch_size * nr_of_cards_per_deck, nr_of_neurons_per_dense)
-        deck_with_cards_removed = self.classifier_layer(deck_with_cards_removed)
-        deck_with_cards_removed_softmax = F.softmax(deck_with_cards_removed, dim = 1)
+        predicted_removed_card = self.classifier_layer(predicted_removed_card)
+        predicted_removed_card = F.softmax(predicted_removed_card, dim = 1)
         # -> (batch_size * nr_of_cards_per_deck, input_size)
 
-        return deck_with_cards_removed_softmax, target_cards_softmax, deck_encoding, ranking_encoding
+        predicted_removed_card = self.encoding_layer(predicted_removed_card)
+        rebuilt_decks = deck_with_cards_removed.add(predicted_removed_card)
+        rebuilt_deck_encoding = torch.mean(rebuilt_decks, dim=0)
+
+        return rebuilt_deck_encoding, ranking_encoding
     
-    def compute_loss(self, deck_with_card_removed, missing_card, deck_encoding, ranking_encoding):
-        loss = 0.0
-        loss += self.missing_card_recovery_loss(deck_with_card_removed, missing_card)/self.nr_of_cards_per_deck
-        loss += self.deck_similarity_to_ranking_loss(deck_encoding, ranking_encoding)
+    # def compute_loss(self, rebuilt_deck_encoding, ranking_encoding):
+    #     loss = self.deck_similarity_to_ranking_loss(rebuilt_deck_encoding, ranking_encoding)
+    #     return loss
+
+    def compute_loss(self, rebuilt_deck_encoding, ranking_encoding):
+        rebuilt_deck_encoding = rebuilt_deck_encoding - torch.min(rebuilt_deck_encoding)
+        ranking_encoding = ranking_encoding - torch.min(ranking_encoding)
+        loss = torch.abs((rebuilt_deck_encoding - ranking_encoding).sum())
         return loss
 
     def get_encoding(self, card_idx):
@@ -116,10 +125,10 @@ optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 loss = 0
 for t in range(EPOCHS):
     # Forward pass
-    deck_with_card_removed, missing_card, deck_encoding, ranking_encoding = model(cards, rankings)
+    rebuilt_deck, ranking_encoding = model(cards, rankings)
 
     # Compute and print loss
-    loss = model.compute_loss(deck_with_card_removed, missing_card, deck_encoding, ranking_encoding)
+    loss = model.compute_loss(rebuilt_deck, ranking_encoding)
 
     if t % 10 == 9:
         print(t, loss.item())
